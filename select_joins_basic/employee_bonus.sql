1b4cf2b0a6924fc50dff0ad4584516056a945188
seoyeong98 <youngseogimkk@gmail.com>
2023-12-11 14:27:14 +0900

    employee_bonus 쿼리 최적화

--- select_joins_basic/employee_bonus.sql
+++ select_joins_basic/employee_bonus.sql
@@ -4,3 +4,11 @@ from
     from Employee
     left join Bonus on Employee.empId = Bonus.empId) as bonus_amt
 where bonus < 1000 or bonus is null
+
+# Write your MySQL query statement below
+select tbl1.name, Bonus.bonus
+from
+(select Employee.name as name, Employee.empId as empId
+from Employee
+where empId not in (select empId from Bonus where bonus>=1000)) as tbl1
+left join Bonus on tbl1.empId = Bonus.empId
